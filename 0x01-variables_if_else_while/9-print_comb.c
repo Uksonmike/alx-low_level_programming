f0037b71684a0c0612810df0a4a718c4a5850765
uksonmike <michaelukpongson91@gmail.com>
2021-08-27 14:17:08 -0700

    print comb.c file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Print single digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 48; m < 58; m++)
+	{
+		putchar(m);
+		if (m != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
