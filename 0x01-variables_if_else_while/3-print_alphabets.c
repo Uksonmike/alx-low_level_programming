a97ca3522c55767a3d116b7fb3865d89e1e433c9
uksonmike <michaelukpongson91@gmail.com>
2021-08-27 12:57:07 -0700

    print alphabet script

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabet in lower and upper case
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+		putchar('\n');
+	return (0);
+}
