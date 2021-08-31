18f63950d916de985a9f56d2d012b6c2c9be3b4b
uksonmike <michaelukpongson91@gmail.com>
2021-08-31 16:24:03 -0700

    24 hours script

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - Last digit
+ * @n: input number as an integer.
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int nv;
+
+	if (n < 0)
+	{
+		nv = -1 * (n % 10);
+		_putchar(nv + '0');
+		return (nv);
+	}
+	else
+	{
+		nv = n % 10;
+		_putchar(nv + '0');
+		return (nv);
+	}
