d771fddd5acdefb65a4508696991ad98742e3a7a
uksonmike <michaelukpongson91@gmail.com>
2021-08-31 16:07:40 -0700

    sign script

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_sign - Checks for sign of numbers
+ * @n: The number to be checked
+ * Return: Different values
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar('\n');
+}
