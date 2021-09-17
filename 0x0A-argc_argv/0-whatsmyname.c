731b64324a86921e43d9cc9d9364f032f7bb971d
uksonmike <michaelukpongson91@gmail.com>
2021-09-17 13:36:07 -0700

    0x0A-argc_argv'

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints program's name, followed by a new line
+ * @argc: Argument count.
+ * @argv: Argument vector(array)
+ * Return: 0
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+**Markdown goes here**
