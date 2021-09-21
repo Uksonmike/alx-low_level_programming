9d1a13fb77e6970b698480ac853f2a3000e3f2c6
uksonmike <michaelukpongson91@gmail.com>
2021-09-21 13:55:30 -0700

    go

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,18 +10,18 @@
 
 char *create_array(unsigned int size, char c)
 {
-  char *a;
-  unsigned int i;
+char *a;
+unsigned int i;
 
-  if (size == 0)
-    return (NULL);
+if (size == 0)
+return (NULL);
 
-  a = malloc(size * sizeof(*a));
-  if (a == NULL)
-    return (NULL);
+a = malloc(size * sizeof(*a));
+if (a == NULL)
+return (NULL);
 
-  for (i = 0; i < size; i++)
-    a[i] = c;
+for (i = 0; i < size; i++)
+a[i] = c;
 
-  return (a);
+return (a);
 }
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -9,9 +9,9 @@
 
 void free_grid(int **grid, int height)
 {
-  int i;
+int i;
 
-  for (i = 0; i < height; i++)
-    free(grid[i]);
-  free(grid);
+for (i = 0; i < height; i++)
+free(grid[i]);
+free(grid);
 }
